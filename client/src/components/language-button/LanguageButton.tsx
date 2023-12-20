17bf9866d89fa70eb39754603b3b0e6756592452
Leon Suarez <leonsua@gmail.com>
2023-12-20 14:47:33 -0500

    created handleOnSwitchLanguage function

--- client/src/components/language-button/LanguageButton.tsx
+++ client/src/components/language-button/LanguageButton.tsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+
 import { LanguageButtonStyled, LanButtonInsideSwitch } from './styled';
 
+import { useTranslation } from 'react-i18next';
+
 export default function LanguageButton() {
+  const [ languageSwitch, setLanguageSwitch ] = useState<string>('en');
+  const { i18n } = useTranslation();
+
+  const currentLanguage = i18n.language;
+  const handleOnSwitchLanguage = (lang: string) => {
+    if (lang === 'es') {
+      setLanguageSwitch('en');
+    } else if (lang === 'en') {
+      setLanguageSwitch('es');
+    }
+     i18n.changeLanguage(languageSwitch)
+  }
+
   return (
     <LanguageButtonStyled>
-      <LanButtonInsideSwitch><span>Español</span></LanButtonInsideSwitch>
+      <LanButtonInsideSwitch onClick={() => handleOnSwitchLanguage(currentLanguage)}><span>Español</span></LanButtonInsideSwitch>
     </LanguageButtonStyled>
   ) 
 }
