46b9f330817f3cc2027fcc1ef3f232c33c6818ce
Leon Suarez <leonsua@gmail.com>
2024-05-31 17:58:00 -0400

    developed the basic menu page layout

--- client/src/views/menu/Menu.tsx
+++ client/src/views/menu/Menu.tsx
@@ -1,9 +1,40 @@
-const Menu = () => {
+import { type FC } from "react";
+
+import {
+  MenuLayout,
+  ImageContainer,
+  HeaderImage,
+  MenuWrapper,
+  MenuDisplay,
+  MenuTitleHolder
+} from "./styled.ts";
+
+const Menu: FC = () => {
   return (
-    <main>
-      <h1>Menu</h1>
-    </main>
-  )
-}
+    <MenuLayout>
+      <ImageContainer>
+        <HeaderImage src="./tamales.webp" />
+      </ImageContainer>
+      <MenuWrapper>
+        <MenuDisplay>
+          <MenuTitleHolder  />
+          <h1>hola</h1>
+        </MenuDisplay>
+      </MenuWrapper>
+      <ImageContainer>
+        <HeaderImage src="./platillo-tipico.jpeg" />
+      </ImageContainer>
+      <MenuWrapper>
+        <h1>hola</h1>
+      </MenuWrapper>
+      <ImageContainer>
+        <HeaderImage src="./Mexican-Cuisine.jpeg" />
+      </ImageContainer>
+      <MenuWrapper>
+        <h1>hola</h1>
+      </MenuWrapper>
+    </MenuLayout>
+  );
+};
 
 export default Menu;
---
+++ client/src/views/menu/styled.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const MenuLayout = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  overflow: hidden;
+`;
+
+export const HeaderImage = styled.img`
+  width: 100%;
+`;
+
+//Menu
+export const MenuWrapper = styled.section`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MenuDisplay = styled.div`
+  width: 80%;
+`;
+
+export const MenuTitleHolder = styled.div``;
+//Menu
